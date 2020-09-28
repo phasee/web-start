5baa77a74a45ee98a94eaf1310e72c1be57b0b02
Stephen <stephen@stoobo.com>
2020-09-27 20:53:01 -0400

    remove api

--- src/services/base.js
+++ src/services/base.js
@@ -1,9 +1,9 @@
 import firebase from 'firebase/app'
 
 var config = {
-  apiKey: 'AIzaSyD7Y2EY-NC4QOfCy6ZTF2_0wipno20frYk',
-  authDomain: 'giggity-86f3b.firebaseapp.com',
-  projectId: 'giggity-86f3b'
+  apiKey: 'API-KEY',
+  authDomain: 'PROJECT-ID.firebaseapp.com',
+  projectId: 'PROJECT-ID'
 }
 
 // Get a Firestore instance
